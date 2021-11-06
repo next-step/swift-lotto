9454ae745a911bbc2ce34c7189b5b4b2bfba6571
Mephrine <dthwrld@gmail.com>
2021-11-06 21:17:39 +0900

    test: Split the input string into an array

---
+++ Lotto/LottoTest/StringAdditionCalcuatorTest.swift
@@ -0,0 +1,18 @@
+//
+//  StringAdditionCalcuatorTest.swift
+//  LottoTest
+//
+//  Created by Mephrine on 2021/11/06.
+//
+
+import XCTest
+
+class StringAdditionCalcuatorTest: XCTestCase {
+	func test_shouldBeAnArrayOfNumbersWhenInputStringIsSplitted() throws {
+		let inputs = "1, 2"
+		let calculator = Calculator()
+		let result = calculator.split(inputs)
+		XCTAssertEqual(result, ["1", "2"])
+	}
+}
+
