e62075f02f009df2d250ce019725de04e19227bb
Mephrine <dthwrld@gmail.com>
2021-11-07 16:40:22 +0900

    feat: create a RandomNumberGenerate and apply it to LottoMachine

---
+++ Lotto/Lotto/Lotto/LottoOption.swift
@@ -0,0 +1,13 @@
+//
+//  LottoOption.swift
+//  Lotto
+//
+//  Created by Mephrine on 2021/11/07.
+//
+
+import Foundation
+
+enum LottoOption {
+	static let randomNumberRange: ClosedRange = 1...45
+	static let numberOfLottoNumbers: Int = 6
+}
