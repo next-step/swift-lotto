8cce864046c8511e2b0493426d9324c829992853
Mephrine <dthwrld@gmail.com>
2021-11-08 00:56:27 +0900

    feat: Implement the winning Lotto

--- Lotto/Lotto/Lotto/Models/Lotto.swift
+++ Lotto/Lotto/Lotto/Models/Lotto.swift
@@ -55,3 +55,28 @@ fileprivate extension String {
 			}
 	}
 }
+
+extension Lotto {
+	func checkWinningRanking(with winningNumbers: Lotto) -> Winnings {
+		let numberOfMatchingNumbers = findNumberOfMatchingNumbers(with: winningNumbers)
+		switch numberOfMatchingNumbers {
+		case 6:
+			return Winnings.firstPlace
+		case 5:
+			return Winnings.secondPlace
+		case 4:
+			return Winnings.thirdPlace
+		case 3:
+			return Winnings.fourthPlace
+		default:
+			return Winnings.notWinning
+		}
+	}
+	
+	private func findNumberOfMatchingNumbers(with winningNumbers: Lotto) -> Int {
+		self.numbers
+			.filter { lottoNumber in
+				winningNumbers.numbers.contains(lottoNumber)
+			}.count
+	}
+}
