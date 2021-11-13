e24aa356556d3d7406ba409ec7f284e3a06d9dc9
BeomKey <nkb5813@naver.com>
2021-11-13 23:19:15 +0900

    입력값이 nil이거나 empty일 때의 테스트케이스 작성

--- Lotto/StringCalculatorTest/StringCalculatorTest.swift
+++ Lotto/StringCalculatorTest/StringCalculatorTest.swift
@@ -6,16 +6,28 @@
 //
 
 import XCTest
+@testable import StringCalculator
 
 class StringCalculatorTest: XCTestCase {
+    var calculator: StringCalculator!
 
     override func setUpWithError() throws {
-        
+        calculator = StringCalculator()
     }
 
     override func tearDownWithError() throws {
-        
+        calculator = nil
     }
     
+    func test_input_string_validation_when_empty_return_0() {
+        let inputString: String? = ""
+        let result = calculator.plus(input: inputString)
+        XCTAssertEqual(result, 0)
+    }
     
+    func test_input_string_validation_when_nil_return_0() {
+        let inputString: String? = nil
+        let result = calculator.plus(input: inputString)
+        XCTAssertEqual(result, 0)
+    }
 }
