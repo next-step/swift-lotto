d298befca277bf291cf0e4487f8fad9df0574fcd
sangsun <hiddenparts@gmail.com>
2021-11-17 01:59:20 +0900

    [1단계] 두번째 테스트 케이스 작성
    - 숫자 하나만 입력했을 경우 해당 숫자를 리턴

--- Lotto/LottoTest/StringCalculatorTest.swift
+++ Lotto/LottoTest/StringCalculatorTest.swift
@@ -12,7 +12,18 @@ class StringCalculatorTest: XCTestCase {
     func test_빈문자열을_입력하면_0을_리턴한다() {
         let calculator = StringCalculator()
         let result = calculator.processingString(by: "")
-        
         XCTAssertEqual(result, 0)
+        
+        let result1 = calculator.processingString(by: "1")
+        XCTAssertNotEqual(result1, 0)
+    }
+    
+    func test_숫자하나만_문자열로_입력하면_해당_숫자를_리턴한다() {
+        let calculator = StringCalculator()
+        let result = calculator.processingString(by: "1")
+        XCTAssertEqual(result, 1)
+        
+        let result1 = calculator.processingString(by: "2")
+        XCTAssertEqual(result1, 2)
     }
 }
