7a4aae09732b1d41d8e32d380560ad31fbe8d225
sangsun <hiddenparts@gmail.com>
2021-11-17 02:31:01 +0900

    [1단계] 리팩토링
    - 문자열 처리와 계산을 함께하는 processingString 함수에서 계산만하는 calculateInputs 함수를 분리시킴

--- Lotto/Lotto/StringCalculator.swift
+++ Lotto/Lotto/StringCalculator.swift
@@ -13,16 +13,23 @@ enum StringCalculatorError: Error {
 
 class StringCalculator {
 
-    /// 사용자로부터 입력받은 문자열을 가공한다
-    func processingString(by inputs: String?) throws -> Int {
+    /// 사용자로부터 입력받은 문자열을 계산한다
+    func calculateInputs(_ inputs: String?) throws -> Int {
         guard let inputs = inputs, inputs.isEmpty == false else { return 0 }
+        
+        let numbers = try processingInputs(by: inputs)
+        let sum = numbers.reduce(0) { $0 + $1 }
+        return sum
+    }
+    
+    /// 사용자로부터 입력받은 문자열을 Int형 배열로 리턴한다
+    func processingInputs(by inputs: String) throws -> [Int] {
         let numbers = inputs.components(separatedBy: [",", ":"]).compactMap { Int($0) }
         try numbers.forEach {
             if $0 < 0 {
                 throw StringCalculatorError.negativeInputs
             }
         }
-        let sum = numbers.reduce(0) { $0 + $1 }
-        return sum
+        return numbers
     }
 }
--- Lotto/LottoTest/StringCalculatorTest.swift
+++ Lotto/LottoTest/StringCalculatorTest.swift
@@ -11,36 +11,36 @@ class StringCalculatorTest: XCTestCase {
 
     func test_빈문자열을_입력하면_0을_리턴한다() throws {
         let calculator = StringCalculator()
-        let result = try calculator.processingString(by: "")
+        let result = try calculator.calculateInputs("")
         XCTAssertEqual(result, 0)
         
-        let result1 = try calculator.processingString(by: "1")
+        let result1 = try calculator.calculateInputs("1")
         XCTAssertNotEqual(result1, 0)
     }
     
     func test_숫자하나만_문자열로_입력하면_해당_숫자를_리턴한다() throws {
         let calculator = StringCalculator()
-        let result = try calculator.processingString(by: "1")
+        let result = try calculator.calculateInputs("1")
         XCTAssertEqual(result, 1)
         
-        let result1 = try calculator.processingString(by: "2")
+        let result1 = try calculator.calculateInputs("2")
         XCTAssertEqual(result1, 2)
     }
     
     func test_두개의_숫자를_입력하면_두_숫자의_합을_리턴한다() throws {
         let calculator = StringCalculator()
-        let result = try calculator.processingString(by: "1,2")
+        let result = try calculator.calculateInputs("1,2")
         XCTAssertEqual(result, 3)
     }
     
     func test_구분자를_컴마_이외에_콜론도_사용가능하다() throws {
         let calculator = StringCalculator()
-        let result = try calculator.processingString(by: "1:2")
+        let result = try calculator.calculateInputs("1:2")
         XCTAssertEqual(result, 3)
     }
     
     func test_음수를_포함하여_전달하면_예외가_발생한다() throws {
         let calculator = StringCalculator()
-        XCTAssertThrowsError(try calculator.processingString(by: "-1,2"))
+        XCTAssertThrowsError(try calculator.calculateInputs("-1,2"))
     }
 }
