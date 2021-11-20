c8581b712a977f628db810f80ed6ee7db198a2eb
Mephrine <dthwrld@gmail.com>
2021-11-21 00:17:41 +0900

    style: Move location

--- Lotto/Lotto/Lotto/Winnings.swift
+++ Lotto/Lotto/Lotto/Winnings.swift
@@ -14,19 +14,7 @@ enum Winnings {
 	case fourthPlace
 	case fifthPlace
 	case notWinning
-	
-	static func from(matchingCount: Int,
-									 matchBonus: Bool = false) -> Self {
-		switch matchingCount {
-		case 6: return .firstPlace
-		case 5 where matchBonus: return .secondPlace
-		case 5: return .thirdPlace
-		case 4: return .fourthPlace
-		case 3: return .fifthPlace
-		default: return .notWinning
-		}
-	}
-	
+		
 	var prize: Int {
 		switch self {
 		case .firstPlace: return 2000000000
@@ -52,5 +40,17 @@ enum Winnings {
 	var isMatchingBonusNumber: Bool {
 		if self == .secondPlace { return true }
 		return false
+	}
+	
+	static func from(matchingCount: Int,
+									 matchBonus: Bool = false) -> Self {
+		switch matchingCount {
+		case 6: return .firstPlace
+		case 5 where matchBonus: return .secondPlace
+		case 5: return .thirdPlace
+		case 4: return .fourthPlace
+		case 3: return .fifthPlace
+		default: return .notWinning
+		}
 	}
 }
