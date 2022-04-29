60caad8e572d93c104441e93275842dbc7e00c99
mansa.kim <zombietux@naver.com>
2022-04-29 21:55:30 +0900

    feat: 결과 출력 개체 추가

---
+++ Lotto/Lotto/Calculator/ResultView.swift
@@ -0,0 +1,14 @@
+//
+//  ResultView.swift
+//  Lotto
+//
+//  Created by 카카오VX on 2022/04/29.
+//
+
+import Foundation
+
+struct ResultView {
+    static func printOperand(_ result: Int) {
+        print(result)
+    }
+}
