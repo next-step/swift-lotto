f243ae4e14b3e11f36be8c5ae169dad8cbf6b902
mansa.kim <zombietux@naver.com>
2022-05-03 07:58:45 +0900

    feat: 보너스 번호 포함 2등 출력

--- Lotto/Lotto/Lotto/LottoResultView.swift
+++ Lotto/Lotto/Lotto/LottoResultView.swift
@@ -36,6 +36,7 @@ struct LottoResultView {
         print("3개 일치 (5000원)- \(report.fifth)개")
         print("4개 일치 (50000원)- \(report.fourth)개")
         print("5개 일치 (1500000원)- \(report.third)개")
+        print("5개 일치, 보너스 번호 일치(30000000원) - \(report.second)개")
         print("6개 일치 (2000000000원)- \(report.first)개")
     }
     
