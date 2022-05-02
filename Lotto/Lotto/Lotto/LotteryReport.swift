3006c9262744b44464146e1df99a62b9983a0e57
mansa.kim <zombietux@naver.com>
2022-05-03 07:55:21 +0900

    feat: 보너스 번호가 생기면서 당첨 리포트 개체 추가

---
+++ Lotto/Lotto/Lotto/LotteryReport.swift
@@ -0,0 +1,13 @@
+//
+//  LotteryReport.swift
+//  Lotto
+//
+//  Created by 카카오VX on 2022/05/03.
+//
+
+import Foundation
+
+struct LotteryReport {
+    let matchCount: Int
+    let isMatch: Bool
+}
