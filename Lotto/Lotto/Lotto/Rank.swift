6ed007944906c3e1502569aa9cf15f5ec59aeb71
mansa.kim <zombietux@naver.com>
2022-05-02 18:52:25 +0900

    feat: 매칭된 개수를 당첨금액으로 변환을 담당하는 Rank 개체 추가

---
+++ Lotto/Lotto/Lotto/Rank.swift
@@ -0,0 +1,32 @@
+//
+//  Rank.swift
+//  Lotto
+//
+//  Created by 카카오VX on 2022/04/30.
+//
+
+import Foundation
+
+enum Rank {
+    case first, third, fourth, fifth, miss
+    
+    static func from(matchingCount: Int) -> Self {
+        switch matchingCount {
+        case 6: return .first
+        case 5: return .third
+        case 4: return .fourth
+        case 3: return .fifth
+        default: return .miss
+        }
+    }
+    
+    var reward: Int {
+        switch self {
+        case .first: return 2_000_000_000
+        case .third: return 1_500_000
+        case .fourth: return 50_000
+        case .fifth: return 5_000
+        case .miss: return 0
+        }
+    }
+}
