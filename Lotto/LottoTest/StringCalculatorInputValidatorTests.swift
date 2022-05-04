fa2b84185eff7c220711b952fd9cc6e879e40490
sujin <rkdthd1234@naver.com>
2022-05-04 20:09:28 +0900

    1단계. StringCalculatorInputValidator 테스트 케이스 추가

--- Lotto/LottoTest/StringCalculatorInputValidatorTests.swift
+++ Lotto/LottoTest/StringCalculatorInputValidatorTests.swift
@@ -19,14 +19,157 @@ class StringCalculatorInputValidatorTests: XCTestCase {
         sut = nil
     }
     
-    func test_isValidInput_whenInputIsSinglePositiveInt_beTrue() throws {
+    func test_isValidInput_whenInputIsSinglePositiveInt_toBeTrue() throws {
         //given
-        let input = "3"
+        let input: String = "3"
         
         // when
-        let result = sut.isValid(of: input)
+        let result: Bool = sut.isValid(of: input)
         
         // then
         XCTAssertTrue(result)
+    }
+    
+    func test_isValidInput_whenUseSeparatorsAsComma_toBeTrue() throws {
+        //given
+        let input: String = "1,2,3"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertTrue(result)
+    }
+    
+    func test_isValidInput_whenUseSeparatorsAsColon_toBeTrue() throws {
+        //given
+        let input: String = "1:2:3"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertTrue(result)
+    }
+    
+    func test_isValidInput_whenUseSeparatorsAsCommaOrColon_toBeTrue() throws {
+        //given
+        let input: String = "1,2:3"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertTrue(result)
+    }
+    
+    func test_isValidInput_whenInputIsEmpty_toBeFalse() throws {
+        //given
+        let input: String = ""
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isValidInput_whenInputIsNil_toBeFalse() throws {
+        //given
+        let input: String? = nil
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isValidInput_whenInputIsSingleNegativeInt_toBeFalse() throws {
+        //given
+        let input: String = "-3"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isValidInput_whenInputIsDouble_toBeFalse() throws {
+        //given
+        let input: String = "3.2"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isValidInput_whenInputHasSameSuccessivieSeparators_toBeFalse() throws {
+        //given
+        let input: String = "1,2,,3"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isValidInput_whenInputHasDifferentSuccessivieSeparators_toBeFalse() throws {
+        //given
+        let input: String = "1,2,:3"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isValidInput_whenInputContainsNegativeInt_toBeFalse() throws {
+        //given
+        let input: String = "1,2,-3"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isValidInput_whenInputContainsNeitherNumberNorSeparator_toBeFalse() throws {
+        //given
+        let input: String = "1,2#3"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isValidInput_whenInputDoesNotEndWithNumber_toBeFalse() throws {
+        //given
+        let input: String = "1,2,"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isValidInput_whenInputContainsSpace_toBeFalse() throws {
+        //given
+        let input: String = "1, 2"
+        
+        // when
+        let result: Bool = sut.isValid(of: input)
+        
+        // then
+        XCTAssertFalse(result)
     }
 }
