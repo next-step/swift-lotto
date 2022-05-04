8002eb35e17e151483c72aaf6008d07bf3bf102f
sujin <rkdthd1234@naver.com>
2022-05-05 01:44:39 +0900

    1단계. StringCalculator 의 calculate 함수 작성

--- Lotto/Lotto/StringCalculator.swift
+++ Lotto/Lotto/StringCalculator.swift
@@ -13,7 +13,50 @@ struct StringCalculator {
         case invalidInput
     }
     
+    private enum Separator: String, CaseIterable {
+        case colon = ":"
+        case comma = ","
+    }
+    
+    private let operandFilter = OperandFilter()
+    private let operandValidator = OperandValidator()
+    
     func calculate(of input: String?) throws -> Int {
-        return 0
+        guard let input = input else {
+            throw StringCalculatorError.invalidInput
+        }
+        
+        let operands = try operands(from: input)
+        
+        guard isAllOperandsValid(operands: operands) else {
+            throw StringCalculatorError.invalidInput
+        }
+        
+        return sum(of: operands)
+    }
+    
+    private func operands(from input: String) throws -> [Int] {
+        let separators: CharacterSet = {
+            let joinedSeparators: String = Separator.allCases
+                .map { separator in
+                    separator.rawValue
+                }
+                .joined()
+            return CharacterSet(charactersIn: joinedSeparators)
+        }()
+        let operands: [Int] = try operandFilter.filter(from: input,
+                                                       separatedBy: separators)
+        return operands
+    }
+    
+    private func isAllOperandsValid(operands: [Int]) -> Bool {
+        let isAllOperandsValid = operands.allSatisfy { operand in
+            operandValidator.isValid(of: operand)
+        }
+        return isAllOperandsValid
+    }
+    
+    private func sum(of operands: [Int]) -> Int {
+        return operands.reduce(0, +)
     }
 }
