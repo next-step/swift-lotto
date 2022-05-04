53250dae0a0050183d7c0894552c9f8c7a37730f
YuchanSong <dvlpr_uchan@kakao.com>
2022-05-04 16:50:24 +0900

    step1: nil 입력에 대한 테스트 케이스 추가

--- Lotto/StringCalculatorTests/StringCalculatorTests.swift
+++ Lotto/StringCalculatorTests/StringCalculatorTests.swift
@@ -8,5 +8,16 @@
 import XCTest
 
 class StringCalculatorTests: XCTestCase {
-
+    
+    func test_nil값이_0을_리턴하는지_검증() throws {
+        // given
+        let input: String? = nil
+        
+        // when
+        let result: Int = try Validator.validNumber(from: input)
+        
+        // then
+        XCTAssertEqual(result, 0)
+    }
+    
 }
