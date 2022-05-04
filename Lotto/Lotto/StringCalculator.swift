26db719e9c0aa500d285cca87c0248363cbed6e2
sujin <rkdthd1234@naver.com>
2022-05-04 23:50:19 +0900

    1단계. StringCalculator 테스트케이스 작성

---
+++ Lotto/Lotto/StringCalculator.swift
@@ -0,0 +1,19 @@
+//
+//  StringCalculator.swift
+//  Lotto
+//
+//  Created by 강수진 on 2022/05/04.
+//
+
+import Foundation
+
+struct StringCalculator {
+    
+    enum StringCalculatorError: Error {
+        case invalidInput
+    }
+    
+    func calculate(of input: String?) throws -> Int {
+        return 0
+    }
+}
---
+++ Lotto/LottoTest/StringCalculatorTests.swift
@@ -0,0 +1,213 @@
+//
+//  StringCalculatorTests.swift
+//  LottoTest
+//
+//  Created by 강수진 on 2022/05/04.
+//
+
+import XCTest
+
+class StringCalculatorTests: XCTestCase {
+
+    var sut: StringCalculator!
+    
+    override func setUpWithError() throws {
+        sut = StringCalculator()
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_calculate_whenInputIsSinglePositiveInt_eqaulToInput() throws {
+        //given
+        let input: String = "3"
+        
+        // when
+        let result: Int = try sut.calculate(of: input)
+        
+        // then
+        let expectation: Int = 3
+        XCTAssertEqual(result, expectation)
+    }
+
+    func test_calculate_whenPositiveIntsAreJoinedWithComma_addAllInts() throws {
+        //given
+        let input: String = "1,2,3"
+
+        // when
+        let result: Int = try sut.calculate(of: input)
+        
+        // then
+        let expectation: Int = 6
+        XCTAssertEqual(result, expectation)
+    }
+
+    func test_calculate_whenPositiveIntsAreJoinedWithColon_addAllInts() throws {
+        //given
+        let input: String = "1:2:3"
+
+        // when
+        let result: Int = try sut.calculate(of: input)
+        
+        // then
+        let expectation: Int = 6
+        XCTAssertEqual(result, expectation)
+    }
+
+    func test_calculate_whenPositiveIntsAreJoinedWithCommaAndColon_addAllInts() throws {
+        //given
+        let input: String = "1,2:3"
+
+        // when
+        let result: Int = try sut.calculate(of: input)
+        
+        // then
+        let expectation: Int = 6
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_calculate_whenInputIsEmpty_throwInvalidInput() throws {
+        //given
+        let input: String = ""
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputIsSpace_throwInvalidInput() throws {
+        //given
+        let input: String = " "
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_calculate_whenInputIsNil_throwInvalidInput() throws {
+        //given
+        let input: String? = nil
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputIsSingleNegativeInt_throwInvalidInput() throws {
+        //given
+        let input: String = "-3"
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputIsDouble_throwInvalidInput() throws {
+        //given
+        let input: String = "3.2"
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputHasSameSuccessivieSeparators_throwInvalidInput() throws {
+        //given
+        let input: String = "1,2,,3"
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputHasDifferentSuccessivieSeparators_throwInvalidInput() throws {
+        //given
+        let input: String = "1,2,:3"
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputContainsNegativeInt_throwInvalidInput() throws {
+        //given
+        let input: String = "1,2,-3"
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputContainsNeitherIntNorSeparator_throwInvalidInput() throws {
+        //given
+        let input: String = "1,2#3"
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputDoesNotEndWithInt_throwInvalidInput() throws {
+        //given
+        let input: String = "1,2,"
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputEndsWithSpace_throwInvalidInput() throws {
+        //given
+        let input: String = "1,2 "
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+    
+    func test_isValidInput_whenInputContainsSpace_throwInvalidInput() throws {
+        //given
+        let input: String = "1, 2"
+        
+        // when
+        // then
+        let expectation = StringCalculator.StringCalculatorError.invalidInput
+        XCTAssertThrowsError(try sut.calculate(of: input)) { error in
+            XCTAssertEqual(error as? StringCalculator.StringCalculatorError, expectation)
+        }
+    }
+}
