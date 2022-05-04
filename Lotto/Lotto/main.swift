fb76da1d59be01f7386322119849618b9b7ff3b5
YuchanSong <dvlpr_uchan@kakao.com>
2022-05-04 17:31:16 +0900

    step1: 메인 테스트 코드 수정

--- Lotto/Lotto/main.swift
+++ Lotto/Lotto/main.swift
@@ -7,7 +7,7 @@
 import Foundation
 
 do {
-    let inputStr: String = "123"
+    let inputStr: String = "1,2:3"
     let inputStrArray: Array<String> = inputStr.components(separatedBy: CharacterSet(charactersIn: ",:"))
     let integerArray: Array<Int> = try inputStrArray.map { (value) in
         return try Validator.validNumber(from: value)
