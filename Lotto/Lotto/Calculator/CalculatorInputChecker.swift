7ddeda3dd64ff54a04063dd9089ff862b2a7ab2a
mansa.kim <zombietux@naver.com>
2022-05-04 21:25:11 +0900

    feat: 지원하지 않는 구분자 에러처리 추가

--- Lotto/Lotto/Calculator/CalculatorInputChecker.swift
+++ Lotto/Lotto/Calculator/CalculatorInputChecker.swift
@@ -5,10 +5,20 @@
 //  Created by 카카오VX on 2022/04/29.
 //
 
+import Foundation
+
 struct CalculatorInputChecker {
     static func validateContainsNagativeNumbers(to splitedExpression: [Int]) throws {
         try splitedExpression.forEach { operand in
-            guard operand > 0 else { throw CalculatorError.unSupportedNegativeNumber }
+            guard operand >= 0 else { throw CalculatorError.unSupportedNegativeNumber }
         }
+    }
+    
+    static func validateContainsUnSupportedSeparator(to splitedExpression: [String]) throws -> [Int] {
+        try splitedExpression.forEach { operand in
+            guard Int(operand) != nil else { throw CalculatorError.unSupportedSeparator }
+        }
+        
+        return splitedExpression.map { Int($0) ?? 0 }
     }
 }
