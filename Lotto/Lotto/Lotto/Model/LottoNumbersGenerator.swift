d8a7e0a88a535c6850a0d643873a2f1d8a9df3d9
sujin <rkdthd1234@naver.com>
2022-05-07 08:06:49 +0900

    2단계. LottoNumberGenerator 에서 numbers 타입을 Set 으로 변경해서 중복 체크

--- Lotto/Lotto/Lotto/Model/LottoNumbersGenerator.swift
+++ Lotto/Lotto/Lotto/Model/LottoNumbersGenerator.swift
@@ -9,15 +9,11 @@ import Foundation
 
 struct LottoNumbersGenerator {
     func generate() -> [Int] {
-        var numbers: [Int] = []
-        while (numbers.count < LottoNumbersValidator.Constants.count) {
-            let numberCandidate = Int.random(in: LottoNumbersValidator.Constants.range)
-            let isNumberUnique = !numbers.contains(numberCandidate)
-            guard isNumberUnique else {
-                continue
-            }
-            numbers.append(numberCandidate)
+        var lottoNumbers: Set<Int> = []
+        while (lottoNumbers.count < LottoNumbersValidator.Constants.count) {
+            let randomLottoNumber = Int.random(in: LottoNumbersValidator.Constants.range)
+            lottoNumbers.insert(randomLottoNumber)
         }
-        return numbers
+        return Array(lottoNumbers)
     }
 }
