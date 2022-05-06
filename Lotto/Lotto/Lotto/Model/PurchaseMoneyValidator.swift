196c51455b7894658c5fd68c9edf1bfe41f782c3
sujin <rkdthd1234@naver.com>
2022-05-07 02:41:22 +0900

    2단계. PurchaseMoneyValidator 작성

--- Lotto/Lotto/Lotto/Model/PurchaseMoneyValidator.swift
+++ Lotto/Lotto/Lotto/Model/PurchaseMoneyValidator.swift
@@ -14,9 +14,19 @@ struct PurchaseMoneyValidator {
         case notUnitOfLotto
     }
     
+    private enum Constants {
+        static let lottoPrice: Int = 1000
     }
     
     func validate(of money: Int) throws {
+        let isEnoughMoneyToBuyLotto: Bool = money >= Constants.lottoPrice
+        guard isEnoughMoneyToBuyLotto else {
+            throw PurchaseMoneyValidatorError.underMinimumMoney
+        }
         
+        let isUnitOfLotto: Bool = money % Constants.lottoPrice == 0
+        guard isUnitOfLotto else {
+            throw PurchaseMoneyValidatorError.notUnitOfLotto
+        }
     }
 }
