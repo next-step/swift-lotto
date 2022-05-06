2720914b08f6a1f3b27f7f190e2c444c3a9de763
sujin <rkdthd1234@naver.com>
2022-05-07 05:49:17 +0900

    2단계. main 함수 작성

--- Lotto/Lotto/Lotto/main.swift
+++ Lotto/Lotto/Lotto/main.swift
@@ -6,5 +6,45 @@
 
 import Foundation
 
-print("Hello, World!")
+do {
+    let inputView = InputView()
+    let purchaseMoney: Int = try inputView.recievePurchaseMoney()
+    let validPurchaseMoney = try validPurchaseMoney(from: purchaseMoney)
 
+    let resultView = ResultView()
+    let purchaseCount = validPurchaseMoney / Lotto.Constants.price
+    resultView.drawPurchaseCount(with: purchaseCount)
+
+    let lottos: [Lotto] = buyLottos(for: purchaseCount)
+    resultView.drawLottos(for: lottos)
+    
+    let winningNumbers: [Int] = try inputView.recieveWinningNumbers()
+    let lottoResult: LottoResult = try lottoResult(lottos: lottos, winningNumbers: winningNumbers)
+    resultView.drawWinningStatistics(with: lottoResult)
+} catch {
+    print(error)
+}
+
+private func validPurchaseMoney(from purchaseMoney: Int) throws -> Int {
+    let purchaseMoneyValidator = PurchaseMoneyValidator()
+    try purchaseMoneyValidator.validate(of: purchaseMoney)
+    return purchaseMoney
+}
+
+private func buyLottos(for purchaseCount: Int) -> [Lotto] {
+    let lottoNumbersGenerator = LottoNumbersGenerator()
+    let lottos: [Lotto] = (1...purchaseCount).map { _ in
+        let lottoNumbers = lottoNumbersGenerator.generate()
+        return Lotto(numbers: lottoNumbers)
+    }
+    return lottos
+}
+
+private func lottoResult(lottos: [Lotto],
+                         winningNumbers: [Int]) throws -> LottoResult {
+    
+    let lottoRankChecker = try LottoRankChecker(winningNumbers: winningNumbers)
+    let lottoResult = LottoResult(lottos: lottos,
+                                  lottoRankChecker: lottoRankChecker)
+    return lottoResult
+}
