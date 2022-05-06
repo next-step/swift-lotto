5eb5e2817267585122d132aae160340e7c92b349
sujin <rkdthd1234@naver.com>
2022-05-07 02:23:49 +0900

    2단계. UserInputConverter 작성

--- Lotto/Lotto/Lotto/Model/UserInputConverter.swift
+++ Lotto/Lotto/Lotto/Model/UserInputConverter.swift
@@ -21,11 +21,39 @@ struct UserInputConverter {
         }
     }
     
+    private let stringConverter = StringConverter()
+    private let splitter = Splitter(separators: [","])
+    
     func convertToMoney(from input: String?) throws -> Int {
-        return 0
+        guard let input = input,
+              !input.isEmpty else {
+            throw UserInputConverterError.PurchaseMoney.empty
+        }
+        
+        do {
+            let money = try stringConverter.convertToInt(from: input)
+            return money
+        } catch {
+            throw UserInputConverterError.PurchaseMoney.notInt
+        }
     }
     
     func convertToWinningNumbers(from input: String?) throws -> [Int] {
-        return [0]
+        guard let input = input,
+              !input.isEmpty else {
+            throw UserInputConverterError.WinningNumbers.empty
+        }
+        
+        do {
+            let components = splitter.components(of: removeSpace(of: input))
+            let winningNumbers = try components.map(stringConverter.convertToInt)
+            return winningNumbers
+        } catch {
+            throw UserInputConverterError.WinningNumbers.extraInputs
+        }
+    }
+    
+    private func removeSpace(of input: String) -> String {
+        return input.replacingOccurrences(of: " ", with: "")
     }
 }
