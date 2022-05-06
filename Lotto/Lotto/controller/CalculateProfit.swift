a55afd7a1a22f0e08914d77b27bde40f69e77b1c
Channy <dvlpr_uchan@kakao.com>
2022-05-07 01:17:31 +0900

    step2: 로또 당첨 결과 및 수익률 출력

---
+++ Lotto/Lotto/controller/CalculateProfit.swift
@@ -0,0 +1,33 @@
+//
+//  CalculateProfit.swift
+//  Lotto
+//
+//  Created by ycsong on 2022/05/07.
+//
+
+import Foundation
+
+class CalculateProfit {
+    
+    let fourth: Int
+    let third: Int
+    let second: Int
+    let first: Int
+    let rate: String
+    
+    init(from lottos: [Lotto]) {
+        self.fourth = lottos.filter { $0.rank == .fourth }.count
+        self.third = lottos.filter { $0.rank == .third }.count
+        self.second = lottos.filter { $0.rank == .second }.count
+        self.first = lottos.filter { $0.rank == .first }.count
+        
+        let amount: Double = Double (fourth * LottoReward.fourth.rawValue
+                                        + third * LottoReward.third.rawValue
+                                        + second * LottoReward.second.rawValue
+                                        + first * LottoReward.first.rawValue)
+
+        self.rate = String(format: "%.2lf",
+                           amount / Double(Constants.lottoPrice * lottos.count))
+    }
+    
+}
--- Lotto/Lotto/view/ResultView.swift
+++ Lotto/Lotto/view/ResultView.swift
@@ -2,11 +2,18 @@
 //  ResultView.swift
 //  Lotto
 //
-//  Created by 송유찬 on 2022/05/06.
+//  Created by ycsong on 2022/05/06.
 //
 
 struct ResultView {
-    static func printStatistics(of lottos: [Lotto]) {
+    static func printResult(of lottos: [Lotto]) {
         print("당첨 통계\n---------")
+        
+        let profit = CalculateProfit(from: lottos)
+        print("3개 일치 (5,000원)- \(profit.fourth)개")
+        print("4개 일치 (50,000원)- \(profit.third)개")
+        print("5개 일치 (1,500,000원)- \(profit.second)개")
+        print("6개 일치 (2,000,000,000원)- \(profit.first)개")
+        print("총 수익률은 \(profit.rate)입니다.")
     }
 }
