e17bcfeffd64d527a6e1215ba36e2a26a10cb921
Channy <dvlpr_uchan@kakao.com>
2022-05-06 23:53:49 +0900

    step2: 로또 생성 로직 추가

---
+++ Lotto/Lotto/controller/LottoGenerator.swift
@@ -0,0 +1,34 @@
+//
+//  LottoGenerator.swift
+//  Lotto
+//
+//  Created by ycsong on 2022/05/06.
+//
+
+class LottoGenerator {
+    
+    private var randomNumber: Int {
+        return Int.random(in: Constants.defaultLottoRange)
+    }
+    
+    private func autoNumbers() -> Lotto {
+        var numbers: Set<Int> = []
+        
+        repeat {
+            numbers.insert(randomNumber)
+        } while numbers.count < Constants.defaultLottoCount
+        
+        return Lotto(numbers: numbers.sorted(by: <))
+    }
+    
+    public func auto(n: Int) -> [Lotto] {
+        var lottos: [Lotto] = []
+        
+        for _ in 0...n {
+            lottos.append(autoNumbers())
+        }
+        
+        return lottos
+    }
+    
+}
