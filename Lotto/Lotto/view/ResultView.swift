008f2cc29fb610a7e0f53d04885f6bfb14d24284
Channy <dvlpr_uchan@kakao.com>
2022-05-06 23:12:20 +0900

    step2: 결과뷰 생성

---
+++ Lotto/Lotto/view/ResultView.swift
@@ -0,0 +1,12 @@
+//
+//  ResultView.swift
+//  Lotto
+//
+//  Created by 송유찬 on 2022/05/06.
+//
+
+struct ResultView {
+    static func printStatistics(of lottos: [Lotto]) {
+        print("당첨 통계\n---------")
+    }
+}
