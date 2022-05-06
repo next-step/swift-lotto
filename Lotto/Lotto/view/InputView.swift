fa8d94905856c0a13ee1225472135398b4cf3b6c
Channy <dvlpr_uchan@kakao.com>
2022-05-06 23:12:11 +0900

    step2: 입력뷰 생성

---
+++ Lotto/Lotto/view/InputView.swift
@@ -0,0 +1,14 @@
+//
+//  InputView.swift
+//  Lotto
+//
+//  Created by ycsong on 2022/05/06.
+//
+
+import Foundation
+
+struct InputView {
+    static func readPurchasePrice() {
+        
+    }
+}
