d68ec0f67a2d095ad87c7bca51e005efd3ad7521
Channy <dvlpr_uchan@kakao.com>
2022-05-07 01:16:49 +0900

    step2: 로또 등수, 보상 enum 추가

--- Lotto/Lotto/model/Lotto.swift
+++ Lotto/Lotto/model/Lotto.swift
@@ -9,10 +9,33 @@ enum LottoError: Error {
     case autoGenerateError
 }
 
+enum LottoRank: Int {
+    case first      = 6
+    case second     = 5
+    case third      = 4
+    case fourth     = 3
+    case unknown    = 0
+    case bang       = -1
+}
+
+enum LottoReward: Int {
+    case first = 2000000000
+    case second = 1500000
+    case third = 50000
+    case fourth = 5000
+}
+
 class Lotto {
     var numbers: [Int]
+    var rank: LottoRank
 
-    init(numbers: [Int]) {
+    init(numbers: [Int], rank: LottoRank = .unknown) {
         self.numbers = numbers
+        self.rank = rank
+    }
+    
+    func match(from winningNumber: [Int]) {
+        let matchCount = numbers.filter { winningNumber.contains($0) }.count
+        self.rank = LottoRank(rawValue: matchCount) ?? .bang
     }
 }
