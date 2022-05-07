0ec840696f7d56b7428debd04f5795ed27aff947
지노지노_정진호 <zino@afreecatv.com>
2022-05-07 15:25:22 +0900

    feat:  불필요한 에러처리 제거

--- Lotto/Lotto/StirngAdditionCaculator/StringAdditionError.swift
+++ Lotto/Lotto/StirngAdditionCaculator/StringAdditionError.swift
@@ -11,7 +11,6 @@ import Foundation
 enum StringAdditionError: Error {
     case notNumber
     case negativeNumber
-    case unallowedDelimiters
 }
 
 extension StringAdditionError: LocalizedError {
@@ -21,8 +20,6 @@ extension StringAdditionError: LocalizedError {
             return "숫자로 변환되지 않는 입력값입니다."
         case .negativeNumber:
             return "음수값이 포함되어있습니다."
-        case .unallowedDelimiters:
-            return "허용되지않는 구분자가 포함되어 있습니다."
         }
     }
 }
