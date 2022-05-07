fadbeed575a0d137135441d7582532bc6ebde28a
sujin <rkdthd1234@naver.com>
2022-05-08 02:56:32 +0900

    3단계. fifth rank 까지 포함한 LottoResult testcase 수정
    # Conflicts:
    #	Lotto/LottoTest/Lotto/LottoResultTests.swift

--- Lotto/LottoTest/Lotto/LottoResultTests.swift
+++ Lotto/LottoTest/Lotto/LottoResultTests.swift
@@ -14,18 +14,66 @@ class LottoResultTests: XCTestCase {
     var secondWinningNumbers: [Int]!
     var thirdWinningNumbers: [Int]!
     var forthWinningNumbers: [Int]!
+    var fifthWinningNumbers: [Int]!
     var noneWinningNumbers: [Int]!
     
     override func setUpWithError() throws {
-        let winningNumbers = [1,2,3,4,5,6]
+        let winningNumber1 = 1
+        let winningNumber2 = 2
+        let winningNumber3 = 3
+        let winningNumber4 = 4
+        let winningNumber5 = 5
+        let winningNumber6 = 6
+        let winningNumbers = [winningNumber1,
+                              winningNumber2,
+                              winningNumber3,
+                              winningNumber4,
+                              winningNumber5,
+                              winningNumber6]
+        
         let bonusNumber = 45
         lottoRankChecker = try LottoRankChecker(winningNumbers: winningNumbers, bonusNumber: bonusNumber)
         
-        firstWinningNumbers = [1,2,3,4,5,6]
-        secondWinningNumbers = [1,2,3,4,5,7]
-        thirdWinningNumbers = [1,2,3,4,7,8]
-        forthWinningNumbers = [1,2,3,7,8,9]
-        noneWinningNumbers = [1,2,7,8,9,10]
+        let noneWinningNumber1 = 7
+        let noneWinningNumber2 = 8
+        let noneWinningNumber3 = 9
+        let noneWinningNumber4 = 10
+        firstWinningNumbers = [winningNumber1,
+                               winningNumber2,
+                               winningNumber3,
+                               winningNumber4,
+                               winningNumber5,
+                               winningNumber6]
+        secondWinningNumbers = [winningNumber1,
+                                winningNumber2,
+                                winningNumber3,
+                                winningNumber4,
+                                winningNumber5,
+                                bonusNumber]
+        thirdWinningNumbers = [winningNumber1,
+                               winningNumber2,
+                               winningNumber3,
+                               winningNumber4,
+                               winningNumber5,
+                               noneWinningNumber1]
+        forthWinningNumbers = [winningNumber1,
+                               winningNumber2,
+                               winningNumber3,
+                               winningNumber4,
+                               noneWinningNumber1,
+                               noneWinningNumber2]
+        fifthWinningNumbers = [winningNumber1,
+                               winningNumber2,
+                               winningNumber3,
+                               noneWinningNumber1,
+                               noneWinningNumber2,
+                               noneWinningNumber3]
+        noneWinningNumbers = [winningNumber1,
+                              winningNumber2,
+                              noneWinningNumber1,
+                              noneWinningNumber2,
+                              noneWinningNumber3,
+                              noneWinningNumber4]
     }
     
     override func tearDownWithError() throws {
@@ -42,7 +90,8 @@ class LottoResultTests: XCTestCase {
         let lottos: [Lotto] = [firstWinningNumbers,
                                secondWinningNumbers,
                                thirdWinningNumbers,
-                               forthWinningNumbers]
+                               forthWinningNumbers,
+                               fifthWinningNumbers]
             .map(Lotto.init)
         
         let sut = LottoResult(lottos: lottos,
@@ -54,6 +103,7 @@ class LottoResultTests: XCTestCase {
         XCTAssertEqual(sut.winningCount(for: .second), 1)
         XCTAssertEqual(sut.winningCount(for: .third), 1)
         XCTAssertEqual(sut.winningCount(for: .forth), 1)
+        XCTAssertEqual(sut.winningCount(for: .fifth), 1)
     }
     
     func test_winningCount_whenGetOnceFirstWinning() throws {
@@ -71,6 +121,7 @@ class LottoResultTests: XCTestCase {
         XCTAssertEqual(sut.winningCount(for: .second), 0)
         XCTAssertEqual(sut.winningCount(for: .third), 0)
         XCTAssertEqual(sut.winningCount(for: .forth), 0)
+        XCTAssertEqual(sut.winningCount(for: .fifth), 0)
     }
     
     func test_winningCount_whenContainsBothValidAndInValidLotto_countValidLotto() throws {
@@ -90,9 +141,9 @@ class LottoResultTests: XCTestCase {
         XCTAssertEqual(firstWinningCount, 1)
     }
     
-    func test_earningsRate_whenBuyFiveLottosAndGetOnceForthWinning_eqaulToOne() throws {
+    func test_earningsRate_whenBuyFiveLottosAndGetOnceFifthWinning_eqaulToOne() throws {
         //given
-        let lottos: [Lotto] = [forthWinningNumbers,
+        let lottos: [Lotto] = [fifthWinningNumbers,
                                noneWinningNumbers,
                                noneWinningNumbers,
                                noneWinningNumbers,
