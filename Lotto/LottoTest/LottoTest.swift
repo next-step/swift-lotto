ee9927ca0442f9aec2d15d5f3a4aaa651041efb2
brianeunseokjeon <qed327@naver.com>
2022-05-07 19:28:00 +0900

    feat: unitest setting 추가, 요구사항 리드미 정리

---
+++ Lotto/LottoTest/LottoTest.swift
@@ -0,0 +1,14 @@
+//
+//  LottoTest.swift
+//  LottoTest
+//
+//  Created by brian은석 on 2022/05/07.
+//
+
+import XCTest
+@testable import Lotto
+
+class LottoTest: XCTestCase {
+
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ### Effective Swift
 NEXTSTEP with yagom-academy
 
+1. 문자열 덧셈 계산기
+- 쉼표 또는 콜론으로 숫자를 구분합니다
+- 분리된 숫자의 합을 반환합니다
+- 숫자 이외의 값 또는 음수는 에러를 반환합니다.
