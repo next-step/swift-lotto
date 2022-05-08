af041f60cdb73e8aea1a024da0a48af63e6c48c9
woosub-lee <woosub.dev@gmail.com>
2022-05-08 21:20:38 +0900

    test: add number seperated by colon

--- Lotto/CalculatorTest/CalculatorTest.swift
+++ Lotto/CalculatorTest/CalculatorTest.swift
@@ -26,5 +26,9 @@ class CalculatorTest: XCTestCase {
     func test_숫자_사이를_컴마로_입력하면_수의_합을_반환() {
         XCTAssertEqual(sut.calculate(with: "1,9"), 10)
     }
+    
+    func test_숫자_사이를_콜론으로_입력하면_수의_합을_반환() {
+        XCTAssertEqual(sut.calculate(with: "1:9"), 10)
+    }
 
 }
