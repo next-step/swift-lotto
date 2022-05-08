23de02566e5ab98e3fac0b7feb7e6b310f2c76a7
woosub-lee <woosub.dev@gmail.com>
2022-05-08 21:04:59 +0900

    test: nil input return

--- Lotto/CalculatorTest/CalculatorTest.swift
+++ Lotto/CalculatorTest/CalculatorTest.swift
@@ -13,5 +13,10 @@ class CalculatorTest: XCTestCase {
         let sut = StringCalculator()
         XCTAssertEqual(sut.calculate(with: ""), 0)
     }
+    
+    func test_입력값이_nil일때_0을_반환() {
+        let sut = StringCalculator()
+        XCTAssertEqual(sut.calculate(with: nil), 0)
+    }
 
 }
