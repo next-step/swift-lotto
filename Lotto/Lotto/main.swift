55be8cfcc65ebb84f876cc134d85bf7e75466d86
RoadtoS7 <kshvv975@gmail.com>
2022-05-08 15:25:37 +0900

    feat: main 로직 구현

--- Lotto/Lotto/main.swift
+++ Lotto/Lotto/main.swift
@@ -6,5 +6,28 @@
 
 import Foundation
 
-print("Hello, World!")
+do {
+    let input = InputView.input()
+    
+    let separators = Separators(value: [Colon(), Comma()])
+    let splitter = Splitter(separaters: separators)
+    let inputParser = InputParser(splitter: splitter, numberGenerator: PositiveNumberGenerator.self)
+    let operands = try inputParser.parse(input: input)
+    
+    var plusOperator = [Operator]()
+    for _ in 0..<operands.count - 1 {
+        plusOperator.append(Plus())
+    }
+    
+    let result = Calculator().calculate(operands: operands, operators: plusOperator)
+    OutputView.showResult(result)
+} catch(let error) {
+    print(error.localizedDescription)
+}
+
+
+
+
+
+
 
