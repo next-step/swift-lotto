b22b148d5f8a4e64d8458d6e8f61f447c39ac55b
RoadtoS7 <kshvv975@gmail.com>
2022-05-08 14:25:04 +0900

    feat: 결과를 출력하는 OutputView 구현

---
+++ Lotto/Lotto/View/OutputView.swift
@@ -0,0 +1,14 @@
+//
+//  OutputView.swift
+//  Lotto
+//
+//  Created by nylah.j on 2022/05/08.
+//
+
+import Foundation
+
+struct OutputView {
+    static func showResult(_ result: Int) {
+        print("결과: \(result)")
+    }
+}
