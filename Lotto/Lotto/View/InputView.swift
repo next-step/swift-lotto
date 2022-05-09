7c9120f205718fad96527f3c0353b3507e896990
RoadtoS7 <kshvv975@gmail.com>
2022-05-09 18:29:20 +0900

    refactor: InputView에서 입력을 받는 메서드 이름 변경
    InputView에서 input보다 readInput이 입력을 받아드린다는 점에서 더 구체적인 메서드명이다

--- Lotto/Lotto/View/InputView.swift
+++ Lotto/Lotto/View/InputView.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct InputView {
-    static func input() -> String? {
+    static func readInput() -> String? {
         print("덧셈할 숫자를 구분자를 기준으로 입력하세요. 구분자는 콜론(:)과 콤마(,)입니다.")
         return readLine()
     }
