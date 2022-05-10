2ccd9a35f79ad70dcc6a055c49094571a671050f
YuchanSong <dvlpr_uchan@kakao.com>
2022-05-10 20:09:34 +0900

    step3: 로또 6자리 테스트 코드 추가

--- Lotto/LottoTests/LottoTests.swift
+++ Lotto/LottoTests/LottoTests.swift
@@ -8,6 +8,30 @@
 import XCTest
 
 class LottoTests: XCTestCase {
+    
+    func test_다섯자리_로또_에러_검증() throws {
+        // given
+        let count = 5
+        
+        // when, then
+        XCTAssertThrowsError(try LottoNumbers(numbers: Array(1...count)))
+    }
+    
+    func test_여섯자리_로또_에러_검증() throws {
+        // given
+        let count = 6
+        
+        // when, then
+        XCTAssertNoThrow(try LottoNumbers(numbers: Array(1...count)))
+    }
+    
+    func test_일곱자리_로또_에러_검증() throws {
+        // given
+        let count = 7
+        
+        // when, then
+        XCTAssertThrowsError(try LottoNumbers(numbers: Array(1...count)))
+    }
 
     func test_로또_한번_자동생성() throws {
         // given
