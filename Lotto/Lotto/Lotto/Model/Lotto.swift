556d60a8c304237b31968a733a64e3348094fb6b
brianeunseokjeon <qed327@naver.com>
2022-05-11 01:08:03 +0900

    feat: 로또 완료

--- Lotto/Lotto/Lotto/Model/Lotto.swift
+++ Lotto/Lotto/Lotto/Model/Lotto.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 struct Lotto {
+    private let randomRange = 1...45
+    let numbers: [Int]
+    var max: Int {
+        return numbers.min() ?? 1
+    }
+    var min: Int {
+        return numbers.max() ?? 45
+    }
     
+    init() {
+        var generateNumbers: Set<Int> = []
+        while generateNumbers.count < 6 {
+            let number = Int.random(in: randomRange)
+            generateNumbers.insert(number)
+        }
+        numbers = Array(generateNumbers).sorted(by: <)
+    }
+
 }
