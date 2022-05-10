52d5e2ed49c9ec987e4c38a0d275be868856147f
RoadtoS7 <kshvv975@gmail.com>
2022-05-10 22:21:41 +0900

    feat: MoneyParserTest에 에러 테스트케이스 추가
    음의 정수가 입력되는 테스트 케이스, 숫자가 아닌 값이 입력되는 테스트 케이스를 추가했다

--- Lotto/LottoTest/MoneyParserTest.swift
+++ Lotto/LottoTest/MoneyParserTest.swift
@@ -15,9 +15,34 @@ class MoneyParserTest: XCTestCase {
         let nilInput: String? = nil
 
         // when
+        // then
         XCTAssertThrowsError(try MoneyParser.parse(money: nilInput), "nil이_입력되면_에러를_발생시킨다") { error in
             XCTAssert(error is MoneyParser.Error)
             XCTAssert(error.localizedDescription ==  MoneyParser.Error.invalidInput(nilInput).errorDescription)
+        }
+    }
+    
+    func test_parse_음의_정수가_입력되면_에러를_발생시킨다() {
+        // given
+        let negativeNumber = "-1"
+
+        // when
+        // then
+        XCTAssertThrowsError(try MoneyParser.parse(money: negativeNumber), "음의정수가_입력되면_에러를_발생시킨다") { error in
+            XCTAssert(error is MoneyParser.Error)
+            XCTAssert(error.localizedDescription ==  MoneyParser.Error.invalidInput(negativeNumber).errorDescription)
+        }
+    }
+    
+    func test_parse_숫자가_아닌값이_입력되면_에러를_발생시킨다() {
+        // given
+        let nonNumber = "*"
+
+        // when
+        // then
+        XCTAssertThrowsError(try MoneyParser.parse(money: nonNumber), "음의정수가_입력되면_에러를_발생시킨다") { error in
+            XCTAssert(error is MoneyParser.Error)
+            XCTAssert(error.localizedDescription ==  MoneyParser.Error.invalidInput(nonNumber).errorDescription)
         }
     }
 }
