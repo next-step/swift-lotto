ace60902c2b7718dfb9fe8e426df74be75b8b809
RoadtoS7 <kshvv975@gmail.com>
2022-05-10 23:36:58 +0900

    feat: LottoSeller sellLotto(for) 실패 테스트 케이스를 통과하는 최소한의 로직 구현

--- Lotto/Lotto/LottoSeller.swift
+++ Lotto/Lotto/LottoSeller.swift
@@ -8,10 +8,24 @@
 import Foundation
 
 struct LottoSeller {
+    enum Error: LocalizedError {
+        case nonEnoughMoney
+        
+        var errorDescription: String? {
+            switch self {
+            case .nonEnoughMoney:
+                return "로또를 사기에 투입된 금액이 부족합니다. 로또 한개의 금액은 \(LottoSeller.lottoPrice)입니다."
+            }
+        }
+    }
+    
     private static let lottoPrice = Money(value: 1000)!
     let lottoTicketMachine: LottoTicketMachine
     
     func sellLotto(for money: Money) throws -> LottoTicket {
+        if money.value < LottoSeller.lottoPrice.value {
+            throw Error.nonEnoughMoney
+        }
         let lottoCount = money / LottoSeller.lottoPrice
         return lottoTicketMachine.printTicket(count: lottoCount)
     }
--- Lotto/Lotto/main.swift
+++ Lotto/Lotto/main.swift
@@ -10,6 +10,13 @@ do {
     let moneyInput = InputView.readMoney()
     let money = try MoneyParser.parse(money: moneyInput)
     
+    let lottoNumberGenerater = RandomLottoNumberGenerator()
+    let lottoFactory = LottoFactory(lottoNumberGenerator: lottoNumberGenerater)
+    let lottoTicketMachine = LottoTicketMachine(lottoFactory: lottoFactory)
+    let lottoSeller = LottoSeller(lottoTicketMachine: lottoTicketMachine)
+    
+    let lottoTicket = try lottoSeller.sellLotto(for: money)
+    
 } catch(let error) {
     
 }
