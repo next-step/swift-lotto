56e9d01dde49f672f9a68207d990d2181aca8cd9
brianeunseokjeon <qed327@naver.com>
2022-05-11 09:36:35 +0900

    feat: 로또 생성기 완료

--- Lotto/Lotto/Lotto/Model/LottoGenerator.swift
+++ Lotto/Lotto/Lotto/Model/LottoGenerator.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 struct LottoGenerator {
+    func generate(count: Int) -> [Lotto] {
+        guard count > 0 else { return [] }
+        var lottos: [Lotto] = []
+        for _ in 1...count {
+            lottos.append(Lotto())
+        }
+        return lottos
+    }
     
+    func moneyToLottoCount(_ money: Int) -> Int {
+        if money < 0 {
+            return 0
+        }
+        return money/1000
+    }
 }
