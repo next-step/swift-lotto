00d2556bb15e75063d34cfc0aecf86867cef9726
RoadtoS7 <kshvv975@gmail.com>
2022-05-12 00:48:49 +0900

    feat: main 로직 구현

--- Lotto/Lotto/main.swift
+++ Lotto/Lotto/main.swift
@@ -16,9 +16,20 @@ do {
     let lottoSeller = LottoSeller(lottoTicketMachine: lottoTicketMachine)
     
     let lottoTicket = try lottoSeller.sellLotto(for: money)
+    let lottoCount = lottoTicket.lottoCount()
+    OutputView.print(lottoCount: lottoCount)
     
+    let formattedLottoTicket = LottoTicketFormatter.format(lottoTicket)
+    OutputView.print(lottoTicket: formattedLottoTicket)
+    
+    let winningLottoInput = InputView.readWinningLotto()
+    let winningLotto = try LottoParser.parse(winningLottoInput)
+    
+    let statistics = lottoTicket.winningStatistics(with: winningLotto)
+    let formattedStatistics = WinningStatisticsFormatter.format(statistics)
+    OutputView.print(winningStatsitics: formattedStatistics)
 } catch(let error) {
-    
+    OutputView.print(error: error)
 }
 
 
