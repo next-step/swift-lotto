59e1a09d548ec333144b74d3d9ae677605cd287c
mansa.kim <zombietux@naver.com>
2022-05-12 19:54:46 +0900

    fix: 메인 함수 수정

--- Lotto/Lotto/main.swift
+++ Lotto/Lotto/main.swift
@@ -10,12 +10,12 @@ func main() {
     do {
         guard let seller = try LottoSeller(purchaseAmount: LottoInputView.readPurchaseAmount(),
                                        lottoMaker: LottoRandomNumberMaker()) else { return }
-        let numberOfManualPurchased = LottoInputView.readNumberOfManualPurchase()
+        let numberOfManualPurchased = LottoInputView.readManualPurchaseCount()
         let manualUserLotto = LottoInputView.readManualLotto(number: numberOfManualPurchased)
         let autoNumber = seller.purchasedNumber() - numberOfManualPurchased
+        let userLotto = try seller.sellLotto(manualNumber: numberOfManualPurchased, manualUserLotto: manualUserLotto)
         LottoResultView.printPurchasedLotto(manualNumber: numberOfManualPurchased,
                                             autoNumber: autoNumber)
-        let userLotto = try seller.sellLotto(manualNumber: numberOfManualPurchased, manualUserLotto: manualUserLotto)
         LottoResultView.printLottos(userLotto)
         let winningLotto = try WinningLottoMaker(lastWeekWinningNumber:
                                                 LottoInputView.readLastWeakWinningNumber(),
@@ -31,7 +31,7 @@ func main() {
     catch let error {
         guard let error = error as? LottoError else { return }
         LottoResultView.printError(error.message ?? "")
-        main()
+        
     }
 }
 
