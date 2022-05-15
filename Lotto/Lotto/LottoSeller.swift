1062ced01a749bc58031b526d59ab364ea0a7880
RoadtoS7 <kshvv975@gmail.com>
2022-05-15 14:08:06 +0900

    refactor: LottoSellr.Error.nonEnoughMoney -> notEnoughMoney

--- Lotto/Lotto/LottoSeller.swift
+++ Lotto/Lotto/LottoSeller.swift
@@ -9,11 +9,11 @@ import Foundation
 
 struct LottoSeller {
     enum Error: LocalizedError {
-        case nonEnoughMoney
+        case notEnoughMoney
         
         var errorDescription: String? {
             switch self {
-            case .nonEnoughMoney:
+            case .notEnoughMoney:
                 return "로또를 사기에 투입된 금액이 부족합니다. 로또 한개의 금액은 \(LottoSeller.lottoPrice)입니다."
             }
         }
@@ -28,7 +28,7 @@ struct LottoSeller {
     }
     
     private func availableLottoCount(with money: Money) throws -> Int {
-        if money < LottoSeller.lottoPrice  { throw Error.nonEnoughMoney }
+        if money < LottoSeller.lottoPrice  { throw Error.notEnoughMoney }
         return money / LottoSeller.lottoPrice
     }
 }
