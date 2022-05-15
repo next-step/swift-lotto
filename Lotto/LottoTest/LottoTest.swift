13085a72a5edcf76713efb49eca72ac15ff64d96
RoadtoS7 <kshvv975@gmail.com>
2022-05-15 16:31:53 +0900

    refactor: LottoTest에 1~45 이내의 숫자만 갖는지 체크하는 유닛 테스트 추가

--- Lotto/LottoTest/LottoTest.swift
+++ Lotto/LottoTest/LottoTest.swift
@@ -18,11 +18,7 @@ class LottoTest: XCTestCase {
 
     func test_init_Lotto를_구성하는_번호개수가_6개가_아니면_에러를_발생시킨다() {
         // given
-        let lottoNumbers = Set([LottoNumber(1),
-                                LottoNumber(2),
-                                LottoNumber(3),
-                                LottoNumber(4),
-                                LottoNumber(5)].compactMap({ $0 }))
+        let lottoNumbers = [1, 2, 3, 4, 5, 6]
 
         // when
         // then
@@ -34,21 +30,8 @@ class LottoTest: XCTestCase {
 
     func test_equalNumberCount_입력받은_Lotto와_일치하는_번호개수를_반환한다() {
             // given
-            let lottoNumbers = Set([LottoNumber(2),
-                                    LottoNumber(3),
-                                    LottoNumber(4),
-                                    LottoNumber(5),
-                                    LottoNumber(6),
-                                    LottoNumber(7)].compactMap({ $0 }))
-            let lotto = try! Lotto(numbers: lottoNumbers)
-
-            let winlottoNumbers = Set([LottoNumber(1),
-                                       LottoNumber(2),
-                                       LottoNumber(3),
-                                       LottoNumber(4),
-                                       LottoNumber(5),
-                                       LottoNumber(6)].compactMap({ $0 }))
-            let winLotto = try! Lotto(numbers: winlottoNumbers)
+            let lotto = try! Lotto(numbers: [2, 3, 4, 5, 6, 7])
+            let winLotto = try! Lotto(numbers: [1, 2, 3, 4, 5, 6])
 
             // when
             let equalNumberCount = lotto.equalNumberCount(with: winLotto)
@@ -56,4 +39,16 @@ class LottoTest: XCTestCase {
             // then
             XCTAssertEqual(equalNumberCount, 5)
         }
+    
+    func test_1에서45이내의_숫자만_갖는다_그외의_숫자가_입력되면_error를_던진다() {
+        // given
+        let lottoNumbers = [1, 45, 0, 46, 2, 3]
+        
+        // when
+        // when
+        XCTAssertThrowsError(try Lotto(numbers: lottoNumbers), "Lotto는 1에서45이내의_숫자만_갖는다_그외의_숫자가_입력되면_error를_던진다") { error in
+            XCTAssert(error is Lotto.Error)
+            XCTAssert(error.localizedDescription == Lotto.Error.invalidLottoNumber.localizedDescription)
+        }
+    }
 }
