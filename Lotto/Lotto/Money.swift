9746589bb7c7f80a39e010d3a3521fd8918429a1
RoadtoS7 <kshvv975@gmail.com>
2022-05-16 00:35:37 +0900

    refactor: Money에서 사용하지 않는 Error 제거

--- Lotto/Lotto/Money.swift
+++ Lotto/Lotto/Money.swift
@@ -8,16 +8,6 @@
 import Foundation
 
 struct Money: Comparable {
-    enum Error: LocalizedError {
-        case invalidValue
-        
-        var errorDescription: String? {
-            switch self {
-            case .invalidValue: return "금액은 양의 정수를 갖습니다."
-            }
-        }
-    }
-    
     static func < (lhs: Money, rhs: Money) -> Bool {
         return lhs.value < rhs.value
     }
