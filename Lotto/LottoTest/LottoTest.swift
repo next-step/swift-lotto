ae749c7f10b943e67faa29b99462b0f22ad4e248
brianeunseokjeon <qed327@naver.com>
2022-05-17 00:04:04 +0900

    2등 테스트 추가

--- Lotto/LottoTest/LottoTest.swift
+++ Lotto/LottoTest/LottoTest.swift
@@ -11,6 +11,7 @@ import XCTest
 class LottoTest: XCTestCase {
     let lottoGenerator = LottoGenerator()
     let winningStatistic = WinningStatistic()
+    let winningNumber = WinningNumber()
     
     func test_1000원미만_300원_입금시_로또0개_생성() {
         let lottoCount = lottoGenerator.moneyToLottoCount(300)
@@ -40,12 +41,19 @@ class LottoTest: XCTestCase {
     }
     
     func test_로또번호_3개일치() {
-        winningStatistic.registerWinningNumbers([1,3,5,7,9,11])
+        winningNumber.register([1,3,5,7,9,11])
         let numbers = [1,2,3,4,5,6]
-        let resultWinning = winningStatistic.match(numbers)
-        let expectedWinning = Winning.fourth
-        
+        let resultWinning = winningNumber.match(numbers)
+        let expectedWinning = Winning.fifth
         XCTAssert(resultWinning == expectedWinning, "로또 번호 3개 일치 실패")
-
+    }
+    
+    func test_로또번호_2등당첨() {
+        winningNumber.register([1,3,5,7,9,11])
+        winningNumber.register(10)
+        let numbers = [1,3,5,7,9,10]
+        let resultWinning = winningNumber.match(numbers)
+        let expectedWinning = Winning.second
+        XCTAssert(resultWinning == expectedWinning, "로또 2등 당첨 실패")
     }
 }
