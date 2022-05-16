c21d3b61f95859ed335c1fb8fb96bace541b1408
RoadtoS7 <kshvv975@gmail.com>
2022-05-16 21:40:17 +0900

    feat: 당첨번호의 보너스번호를 갖는지 체크하는 WinningLotto.doesBonusNumberMathi()의 실패 테스트 케이스 추가

--- Lotto/LottoTest/WinningLottoTest.swift
+++ Lotto/LottoTest/WinningLottoTest.swift
@@ -8,6 +8,28 @@
 import XCTest
 
 class WinningLottoTest: XCTestCase {
-
+    func test_doesMatchBonusLotto_인자로_주어진_Lotto에_bonus번호와_일치하는_번호가_존재하면_true를_반환한다() throws {
+        // given
+        let winningLotto = try WinningLotto(numbers: [1, 2, 3, 4, 5, 6], bonusNumber: 7)
+        let lotto = try DefaultLotto(numbers: [7, 1, 2, 3, 4, 5])
+        
+        // when
+        let result = winningLotto.doesBonusNumberMatch(with: lotto)
+        
+        // then
+        XCTAssertTrue(result)
+    }
+    
+    func test_doesMatchBonusLotto_인자로_주어진_Lotto에_bonus번호와_일치하는_번호가_존재하지_않으면_false를_반환한다() throws {
+        // given
+        let winningLotto = try WinningLotto(numbers: [1, 2, 3, 4, 5, 6], bonusNumber: 7)
+        let lotto = try DefaultLotto(numbers: [1, 2, 3, 4, 5, 6])
+        
+        // when
+        let result = winningLotto.doesBonusNumberMatch(with: lotto)
+        
+        // then
+        XCTAssertTrue(result)
+    }
 
 }
