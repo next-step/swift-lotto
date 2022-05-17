d523dfbeb654e0df554cc1e3e241f96014720196
woosub-lee <woosub.dev@gmail.com>
2022-05-17 23:24:24 +0900

    feat: buy lotto with price

---
+++ Lotto/Lotto/Lotto/LottoCustomer.swift
@@ -0,0 +1,25 @@
+//
+//  LottoCustomer.swift
+//  Lotto
+//
+//  Created by 이우섭 on 2022/05/17.
+//
+
+import Foundation
+
+class LottoCustomer {
+    
+    var lottos = [Lotto]()
+    
+    func buyLotto(for price: Int) {
+        let lottoCount = price / 1000
+        for _ in 0..<lottoCount {
+            let lotto = Lotto()
+            lottos.append(lotto)
+        }
+    }
+}
+
+struct Lotto {
+    
+}
