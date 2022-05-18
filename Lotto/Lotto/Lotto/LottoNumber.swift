6993df36fee6d945fa5175f570164d887019d91c
woosub-lee <woosub.dev@gmail.com>
2022-05-18 12:44:22 +0900

    refactor: divide file structure

---
+++ Lotto/Lotto/Lotto/LottoNumber.swift
@@ -0,0 +1,8 @@
+//
+//  LottoNumber.swift
+//  Lotto
+//
+//  Created by 이우섭 on 2022/05/18.
+//
+
+import Foundation
---
+++ Lotto/Lotto/Lotto/LottoNumbers.swift
@@ -0,0 +1,8 @@
+//
+//  LottoNumbers.swift
+//  Lotto
+//
+//  Created by 이우섭 on 2022/05/18.
+//
+
+import Foundation
