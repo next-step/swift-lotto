e4cbe73780e487a2a3edd35cb66a4dacfd78f881
RoadtoS7 <kshvv975@gmail.com>
2022-05-19 22:29:21 +0900

    feat: LottoNumberValidatorTest에 LottoNumber의 개수를 체크하는 유닛테스트케이스 추가

--- Lotto/LottoTest/LottoNumberValidatorTest.swift
+++ Lotto/LottoTest/LottoNumberValidatorTest.swift
@@ -19,4 +19,15 @@ class LottoNumberValidatorTest: XCTestCase {
         // then
         XCTAssertTrue(result)
     }
+    
+    func test_validateNumberCount_파라미터로_주어진_IntArray가_lotto번호개수만큼_숫자를_가지고있지않으면_false를_반환한다() {
+        // given
+        let invalidLottoNumbers = [1, 2, 3, 4, 5, 5]
+        
+        // when
+        let result = LottoNumberValidator.validate(numbers: invalidLottoNumbers)
+        
+        // then
+        XCTAssertFalse(result)
+    }
 }
