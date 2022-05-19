b6469a9b185d9a7b6c16410612394d701955c412
RoadtoS7 <kshvv975@gmail.com>
2022-05-19 22:15:04 +0900

    feat: CustomLotto 테스트 케이스 추가하는 intializer 로직 구현

--- Lotto/Lotto/Lotto/CustomLotto.swift
+++ Lotto/Lotto/Lotto/CustomLotto.swift
@@ -9,4 +9,23 @@ import Foundation
 
 struct CustomLotto: Lotto {
     var numbers: Set<Int>
-} 
+    
+    init<LottoNumberCollection: Collection>(numbers: LottoNumberCollection) throws where LottoNumberCollection.Element == Int {
+        let numberSet = Set(numbers)
+        guard LottoConstant.numberCount == numberSet.count else {
+            throw LottoError.invalidNumberCount(numberSet.count)
+        }
+        
+        try numberSet.forEach {
+            if LottoConstant.numberRange.contains($0) == false {
+                throw LottoError.invalidLottoNumber
+            }
+        }
+        
+        guard LottoNumberValidator.validate(numbers: numbers) == true else {
+            throw LottoError.invalidLottoNumber
+        }
+        
+        self.numbers = numberSet
+    }
+}
