3c9410fb85314cf5050fecd1eddf8dda3476eb92
RoadtoS7 <kshvv975@gmail.com>
2022-05-20 13:05:53 +0900

    feat: 원시값 포장을 위해 LottoNumber 구현

---
+++ Lotto/Lotto/LottoNumber.swift
@@ -0,0 +1,14 @@
+//
+//  LottoNumber.swift
+//  Lotto
+//
+//  Created by nylah.j on 2022/05/20.
+//
+
+import Foundation
+
+struct LottoNumber {
+    let value: Int
+    
+    init(_ value: Int) { self.value = value }
+}
