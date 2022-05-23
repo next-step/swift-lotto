6b01ec6e20bb82869eacc3bd1d5ff8e5b968ff1c
woosub-lee <woosub.dev@gmail.com>
2022-05-24 00:40:30 +0900

    feat: get prize

--- Lotto/Lotto/Lotto/Lotto.swift
+++ Lotto/Lotto/Lotto/Lotto.swift
@@ -8,6 +8,14 @@
 import Foundation
 
 struct Lotto {
+    enum Prize: Int {
+        case first = 2_000_000_000
+        case second = 1_500_000
+        case third = 50_000
+        case fourth = 5_000
+        case miss = 0
+    }
+    
     var numbers: LottoNumbers
     
     func match(with comparedNumbers: LottoNumbers) -> Int {
@@ -15,5 +23,20 @@ struct Lotto {
         let rhs = comparedNumbers.comparativeSet
         
         return lhs.intersection(rhs).count
+    }
+    
+    func getPrize(by matchNumber: Int) -> Int {
+        switch matchNumber {
+        case 6:
+            return Prize.first.rawValue
+        case 5:
+            return Prize.second.rawValue
+        case 4:
+            return Prize.third.rawValue
+        case 3:
+            return Prize.fourth.rawValue
+        default:
+            return Prize.miss.rawValue
+        }
     }
 }
