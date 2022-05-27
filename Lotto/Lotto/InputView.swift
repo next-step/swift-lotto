c39ce77de80f36d59c5f294f897df34246914a23
KimDuni <7577ksj@gmail.com>
2022-05-27 10:30:35 +0900

    feat: 입력문자에 음수가 섞여있을 경우 error발생하도록 함.

--- Lotto/Lotto/InputView.swift
+++ Lotto/Lotto/InputView.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+enum InputError: Error {
+    case unacceptableNumbers
+}
+
 struct InputView {
     func validation(_ receivedText: String?) -> Int {
         guard let input: String = receivedText,
@@ -16,13 +20,19 @@ struct InputView {
         return 1
     }
     
-    func convertToIntegerArray(receivedText text: String) -> [Int] {
+    func convertToIntegerArray(receivedText text: String) throws -> [Int] {
         if let number: Int = Int(text) {
             return [number]
         }
         
-        return text.replacingOccurrences(of: ":", with: ",")
+        let result = text.replacingOccurrences(of: ":", with: ",")
             .components(separatedBy: ",")
             .compactMap { Int($0) }
+       
+        guard result.filter({ $0 < 0 }).isEmpty else {
+            throw InputError.unacceptableNumbers
+        }
+        
+        return result
     }
 }
--- Lotto/StringCalulator/StringCalulator.swift
+++ Lotto/StringCalulator/StringCalulator.swift
@@ -39,7 +39,7 @@ class StringCalulator: XCTestCase {
         let inputView = InputView()
         
         //when
-        let result = inputView.convertToIntegerArray(receivedText: receivedText)
+        let result = try! inputView.convertToIntegerArray(receivedText: receivedText)
         
         //then
         XCTAssertEqual(result[0], 1)
@@ -51,7 +51,7 @@ class StringCalulator: XCTestCase {
         let inputView = InputView()
         
         //when
-        let result = inputView.convertToIntegerArray(receivedText: receivedText)
+        let result = try! inputView.convertToIntegerArray(receivedText: receivedText)
         
         //then
         XCTAssertEqual(result, [1,2,3,4,5])
@@ -62,36 +62,10 @@ class StringCalulator: XCTestCase {
         //given
         let receivedText: String = "1,2:3,4:-5"
         let inputView = InputView()
-        
+
         //when / then
         XCTAssertThrowsError(try inputView.convertToIntegerArray(receivedText: receivedText)) { error in
-            XCTAssertEqual(error as! InputError.unacceptableNumbers, true)
+            XCTAssertEqual(error as! InputError, InputError.unacceptableNumbers)
         }
     }
-    
-    
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
