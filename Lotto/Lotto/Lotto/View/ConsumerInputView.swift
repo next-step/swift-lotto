b9cf2a5f61c2a562327babad05643d8d4bda42a3
woosub-lee <woosub.dev@gmail.com>
2022-05-31 11:18:56 +0900

    feat: input manual lottos

--- Lotto/Lotto/Lotto/View/ConsumerInputView.swift
+++ Lotto/Lotto/Lotto/View/ConsumerInputView.swift
@@ -10,23 +10,51 @@ import Foundation
 struct ConsumerInputView {
     func readPrice() -> Int? {
         guard let input = readInput(with: "구입금액을 입력해 주세요."), let price = Int(input) else { return nil }
+        print()
         return price
     }
     
-    func readPrizeNumbers() -> LottoNumbers? {
-        guard let input = readInput(with: "지난 주 당첨 번호를 입력해 주세요.") else { return nil }
+    func readManualLottos() -> [LottoNumbers]? {
+        guard let input = readInput(with: "수동으로 구매할 로또 수를 입력해 주세요."),
+              let count = Int(input), count != 0 else { return nil }
         print()
-        let numbers = input.components(separatedBy: ", ")
-            .compactMap(Int.init)
-            .compactMap(LottoNumber.init)
         
+        var manuallyGenerated: [LottoNumbers?] = []
+        for _ in 0..<count {
+            let numbers = readLottoNumbers()
+            manuallyGenerated.append(numbers)
+        }
+        print()
+        
+        let vaildGenerated = manuallyGenerated
+            .compactMap({ $0 })
+            .filter({ $0.count == 6 })
+        
+        guard vaildGenerated.count == count else { return nil }
+        return vaildGenerated
+    }
+    
+    
+    
+    func readPrizeNumbers() -> LottoNumbers? {
+        print("지난 주 당첨 번호를 입력해 주세요.")
+        guard let numbers = readLottoNumbers() else { return nil }
         guard numbers.count == 6 else { return nil }
-        return LottoNumbers(numbers: numbers)
+        return numbers
     }
     
     func readBonusNumber() -> LottoNumber? {
         guard let input = readInput(with: "보너스 번호를 입력해 주세요."), let number = Int(input) else { return nil }
+        print()
         return LottoNumber(number)
+    }
+    
+    private func readLottoNumbers() -> LottoNumbers? {
+        guard let input = readLine() else { return nil }
+        let numbers = input.components(separatedBy: ", ")
+            .compactMap(Int.init)
+            .compactMap(LottoNumber.init)
+        return LottoNumbers(numbers: numbers)
     }
     
     private func readInput(with comment: String) -> String? {
